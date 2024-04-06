1c662b433b9a8688579d436859123f5abc3ded7e
skyEverTerra <everex001@gmail.com>
2024-04-06 17:48:16 -0600

    Base de datos creada

--- games/models.py
+++ games/models.py
@@ -1,3 +1,15 @@
+
+# Django
 from django.db import models
 
+# Models
+from users.models import Alumno
+from users.models import Materia
+
 # Create your models here.
+class Juego(models.Model):
+    id_juego = models.AutoField(primary_key=True)
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    puntos = models.IntegerField()
+    tiempo_realizado = models.DateTimeField()
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
--- users/models.py
+++ users/models.py
@@ -8,3 +8,26 @@ from django.db import models
 class Maestro(models.Model):
     """ Teacher's model"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Escuela(models.Model):
+    id_escuela = models.AutoField(primary_key=True)
+    nombre_escuela = models.CharField(max_length=255)
+
+class Grupo(models.Model):
+    id_grupo = models.AutoField(primary_key=True)
+    nombre_grupo = models.CharField(max_length=255)
+
+class Materia(models.Model):
+    id_materia = models.AutoField(primary_key=True)
+    nombre_materia = models.CharField(max_length=255)
+
+class Alumno(models.Model):
+    id_alumno = models.AutoField(primary_key=True)
+    nom_alumno = models.CharField(max_length=255)
+    nom_padre = models.CharField(max_length=255)
+    escuela = models.ForeignKey(Escuela, on_delete=models.CASCADE)
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+
+class Materia_Maestro(models.Model):
+    id = models.AutoField(primary_key=True)
+    maestro = models.ForeignKey(Maestro, on_delete=models.CASCADE)
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
